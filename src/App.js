aee63262fd79c83924ec0cb17c63c5390859c0ca
haditariq <hadi-tariq@live.co.uk>
2023-02-18 14:19:05 +0100

    project structured intialised

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ScoreBoard from './pages/ScoreBoard';
+
+function App() {
+  return (
+    <div className='App'>
+      <ScoreBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ScoreBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from '../components/Heading';
+
+const ScoreBoard = () => {
+  return (
+    <div>
+      Scoreboard displayes
+      <Heading />
+    </div>
+  );
+};
+
+export default ScoreBoard;
