0767d0a87130331b4508eeebf3e7f21291d3011c
haditariq <hadi-tariq@live.co.uk>
2023-02-18 20:59:51 +0100

    update: button component implementation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
-const Button = () => {
-  return <div>implement me, I'm a button!</div>;
+const Button = ({ disabled, onPress }) => {
+  return (
+    <button disabled={disabled} onClick={onPress}>
+      implement me, I'm a button!
+    </button>
+  );
 };
 
 export default Button;
